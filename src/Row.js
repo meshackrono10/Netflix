3cc9d75b6dd15366fcd593342036f3953c1e5323
Enock <enockkoech@Xenons-MacBook.home>
2021-05-10 13:24:07 +0300

    second commit

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./Row.css";
 import axios from "./axios";
-import movieTrailer from "movie-trailer";
 import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import "./Row.css";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -21,16 +21,18 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   const opts = {
     height: "390",
-    width: "100%",
+    width: "99%",
     playerVars: {
-      autoplay: 1,
+      autoplay: 0,
     },
   };
+
   const handleClick = (movie) => {
+    // console.table(movie?.title)
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.name || "")
+      movieTrailer(movie?.title || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
@@ -43,19 +45,23 @@ function Row({ title, fetchUrl, isLargeRow }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row_poster ${isLargeRow && "row_posterLarge"} `}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          );
+        })}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
     </div>
   );
 }
